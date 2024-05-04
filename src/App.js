d328791e5b673ab226475cc875a75e5cd46d676e
Naveen Burathi <naveenburathi@Naveens-MacBook-Air.local>
2024-05-05 00:40:44 +0530

    Update reset functionality

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ import { useState } from 'react';
 export default function App() {
   const [splitterObj,setSplitterObj]=useState({
     bill:'',
-    customTip:'0',
-    noOfPeople:'',
-    errorNoOfPeople:false
+    customTip:'',
+    noOfPeople:'0',
   })
+
   return <div>
     <Grid className='text-top' container direction={'column'} justifyContent={'center'}  alignItems={'center'}>
       <Grid item>SPLI</Grid>
       <Grid item>TTER</Grid>
     </Grid>
     <FormControl sx={{'width':'100vw'}}>
-    <Grid container direction={'row'} sx={{'padding':{sm:'32px', xs:'20px'},'width':{sm:'70vw !important', xs:'100vw'}}} className='content' justifyContent={'space-between'}  >
+    <Grid container direction={'row'} sx={{'padding':{sm:'32px', xs:'20px'},'width':'70vw !important'}} className='content' justifyContent={'space-between'}  >
       <Grid  item xs='12' md='6' sx={{'margin-bottom':{xs:'5vh',md:'0'}}}><LeftComponent splitterObj={splitterObj} setSplitterObj={setSplitterObj}/></Grid>
       <Grid  item xs='12' md='6'><RightComponent splitterObj={splitterObj} setSplitterObj={setSplitterObj}></RightComponent></Grid>
     </Grid>
--- src/components/rightcomponent.js
+++ src/components/rightcomponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
@@ -6,36 +6,38 @@ import { useState } from 'react';
 
 
 const RightComponent = ({splitterObj, setSplitterObj}) => {
-    const [tipAmount,setTipAmount]= useState('0.00');
-    const [totalAmount,setTotalAmount]= useState('0.00');
-
-    const handleSubmit =()=>{
-        if(splitterObj.noOfPeople==='0'||splitterObj.noOfPeople==='')
-        {
-            setSplitterObj({...splitterObj,errorNoOfPeople:'true'});
-            return;
-        }
-        const bill= parseFloat(splitterObj.bill);
-        const customTip= parseFloat(splitterObj.customTip);
-        const noOfPeople= parseFloat(splitterObj.noOfPeople);
-
-        const totalTipPH=(bill*(customTip/100))/noOfPeople;
-        const totalBillPH=(bill*(customTip/100)+bill)/noOfPeople;
-
-        setTipAmount(totalTipPH.toFixed(2));
-        setTotalAmount(totalBillPH.toFixed(2));
-        // setSplitterObj({bill:'',customTip:'0',noOfPeople:'',errorNoOfPeople:false});
+    const[disableReset,setDisableReset]=useState(true);
+    
+    const handleReset =()=>{
+        setSplitterObj({bill:'',
+        bill:'',
+    customTip:'',
+    noOfPeople:'0',});
     }
 
+    const bill= parseFloat(splitterObj.bill);
+    const customTip= isNaN(parseFloat(splitterObj.customTip))?0:parseFloat(splitterObj.customTip);
+    const noOfPeople= parseFloat(splitterObj.noOfPeople);
+
+    const tipAmount=!isNaN(bill)&&!isNaN(customTip)&&!isNaN(noOfPeople)&&noOfPeople!==0?
+                    (bill*(customTip/100))/noOfPeople:0;
+    const totalAmount=!isNaN(bill)&&!isNaN(customTip)&&!isNaN(noOfPeople)&&noOfPeople!==0?(bill*(customTip/100)+bill)/noOfPeople:0;
+
+    useEffect(()=>{
+        console.log('called');
+        if(splitterObj.noOfPeople==='0')
+            setDisableReset(true);
+        else setDisableReset(false);
+    },[splitterObj])
     return (
         <Grid className='right-content' container direction={'column'} justifyContent={'space-between'}>
             <Grid item container direction={'row'} justifyContent={'space-between'} alignItems={'center'} >
-                <Grid item xs={6} sx={{'color':'hsl(0, 0%, 100%)','padding-top':'10px'}}>Tip Amount<br/><span style={{'color':'hsl(184, 14%, 56%)'}}>/ person</span></Grid>
-                <Grid item xs={6}  sx={{'color':'#2bbba6','font-size':'44px','font-weight':'600','text-align':'right'}}>${tipAmount}</Grid>
-                <Grid item xs={6} sx={{'color':'hsl(0, 0%, 100%)','padding-top':'10px'}}>Total<br/><span style={{'color':'hsl(184, 14%, 56%)'}}>/ person</span></Grid>
-                <Grid item xs={6} sx={{'color':'#2bbba6','font-size':'44px','font-weight':'600','text-align':'right'}}>${totalAmount}</Grid>
+                <Grid item xs={4} sx={{'color':'hsl(0, 0%, 100%)','padding-top':'10px'}}>Tip Amount<br/><span style={{'color':'hsl(184, 14%, 56%)'}}>/ person</span></Grid>
+                <Grid item xs={8}  sx={{'color':'#2bbba6','font-size':'44px','font-weight':'600','text-align':'right'}}>${tipAmount.toFixed(2)}</Grid>
+                <Grid item xs={4} sx={{'color':'hsl(0, 0%, 100%)','padding-top':'10px'}}>Total<br/><span style={{'color':'hsl(184, 14%, 56%)'}}>/ person</span></Grid>
+                <Grid item xs={8} sx={{'color':'#2bbba6','font-size':'44px','font-weight':'600','text-align':'right'}}>${totalAmount.toFixed(2)}</Grid>
             </Grid>
-            <Grid item><Button className='reset-button' variant="contained" onClick={handleSubmit}>RESET</Button></Grid>
+            <Grid item><Button className={disableReset?'disable-reset-button':'reset-button'}  variant="contained" onClick={handleReset}>RESET</Button></Grid>
         </Grid>
     );
 }
